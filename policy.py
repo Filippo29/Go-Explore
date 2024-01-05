bc5e4ccc613473195ae610d945f9723f03bfb04a
Filippo29 <filodev06@gmail.com>
2024-01-05 10:18:11 +0100

    added patience mechanism to help policy convergence

--- policy.py
+++ policy.py
@@ -7,7 +7,7 @@ from environment import Montezuma
 def get_target_reward(trajectory, start_index):
     return sum([trajectory[i][3] for i in range(start_index, len(trajectory))])
 
-def robustification(trajectory, transforms, start_point=0):
+def robustification(trajectory, transforms, start_point=None, max_timesteps=1e2, patience=10):
     if start_point is None:
         start_point = len(trajectory)-10
     deterministic = False
@@ -19,7 +19,8 @@ def robustification(trajectory, transforms, start_point=0):
         max_steps = 5*(len(trajectory)-start_point)
         print("Target reward: ", target_reward)
         
-        args = {"start_position": (trajectory[start_point][0], trajectory[start_point][2]), 
+        start_position = (trajectory[start_point][0], trajectory[start_point][2]) if start_point > 0 else None
+        args = {"start_position": start_position, 
                 "target_reward": target_reward, 
                 "frame_skip": 1,
                 "max_steps": max_steps,
@@ -38,9 +39,20 @@ def robustification(trajectory, transforms, start_point=0):
         else:
             model.set_env(env)
         score = test(Montezuma, transforms, model, trajectory, start_point, target_reward, num_simulations=5, deterministic=deterministic, log=True)
+        epochs_with_no_reward = 0
         while score < target_reward:
-            model.learn(total_timesteps=int(1e2), progress_bar=True)
+            model.learn(total_timesteps=int(max_timesteps), progress_bar=True)
             score = test(Montezuma, transforms, model, trajectory, start_point, target_reward, num_simulations=5, deterministic=deterministic, log=True)
+            if score == 0:
+                epochs_with_no_reward += 1
+            else:
+                epochs_with_no_reward = 0
+            if patience != -1 and epochs_with_no_reward >= patience:
+                break
+        if patience != -1 and epochs_with_no_reward >= patience:
+            model = PPO.load("montezuma_save", env=env, device="mps")
+            start_point += 1
+            continue
         model.save("montezuma_save")
         
         test(Montezuma, transforms, model, trajectory, start_point, target_reward, num_simulations=1, deterministic=deterministic, log=True, render_mode="human")
@@ -56,11 +68,12 @@ def test(env_class, transforms, model, trajectory, start_point, target_reward, m
     env = env_class(render_mode=render_mode, transforms=transforms, max_steps=max_steps, target_reward=target_reward, start_position=(trajectory[start_point][0], trajectory[start_point][2]), deterministic=deterministic)
     predictions = [0 for _ in range(env.action_space.n)]
 
+    max_steps = 5*(len(trajectory)-start_point)
     done = False
     tot_reward = 0
     for _ in range(num_simulations):
         obs = env.reset(0)[0]
-        for _ in range(1000):
+        for _ in range(max_steps):
             action = model.predict(obs)[0]
             predictions[action] += 1
             obs, reward, done, _, _ = env.step(action)
